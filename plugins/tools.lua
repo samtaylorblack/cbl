799d40684c3d3fce18cb7eac6e7d03c3b9bc92c4
samtaylor <samtaylor@tmails.net>
2017-04-17 17:40:51 +0430

    Update tools.lua

--- plugins/tools.lua
+++ plugins/tools.lua
@@ -1192,7 +1192,7 @@ if matches[1] == "helptools" and is_mod(msg) then
 if not lang then
 text = [[
 
-_Sudoer And Admins Beyond Bot Help :_
+_Sudoer And Admins Black Bot Help :_
 
 *!visudo* `[username|id|reply]`
 _Add Sudo_
@@ -1301,7 +1301,7 @@ tdcli.sendMessage(msg.chat_id_, 0, 1, text, 1, 'md')
 else
 
 text = [[
-_راهنمای ادمین و سودو های ربات بیوند:_
+_راهنمای ادمین و سودو های ربات بلک:_
 
 *!visudo* `[username|id|reply]`
 _اضافه کردن سودو_
