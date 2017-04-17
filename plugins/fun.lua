720c21a612038fde04919bf3e4b51b3dec0b13ce
samtaylor <samtaylor@tmails.net>
2017-04-17 17:41:46 +0430

    Update fun.lua

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -314,7 +314,7 @@ local hash = "gp_lang:"..msg.to.id
 local lang = redis:get(hash)
 if not lang then
 helpfun = [[
-_Beyond Reborn Fun Help Commands:_
+_Black Reborn Fun Help Commands:_
 
 *!time*
 _Get time in a sticker_
@@ -361,7 +361,7 @@ tdcli.sendMessage(msg.chat_id_, 0, 1, helpfun, 1, 'md')
 else
 
 helpfun = [[
-_راهنمای فان ربات بیوند:_
+_راهنمای فان ربات بلک:_
 
 *!time*
 _دریافت ساعت به صورت استیکر_
